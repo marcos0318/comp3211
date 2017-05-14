ac3255e03818d5c4f67aab350dad62da1c8de665
Shaohui Yang <ysh@ysh.local>
2017-05-14 17:11:04 +0800

    delete few functions

--- contest/myTeam.py
+++ contest/myTeam.py
@@ -1,4 +1,4 @@
-#Example modifying
+·#Example modifying
 
 
 
@@ -10,7 +10,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -57,8 +57,8 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 # Agents #
 ##########
-#the Planning Cpature is from baseline team. 
-#the baseline team is not very good. And I am going to fix it 
+#the Planning Cpature is from baseline team.
+#the baseline team is not very good. And I am going to fix it
 class PlanningCaptureAgent(CaptureAgent):
   """
   A base class for reflex agents that chooses score-maximizing actions
@@ -79,7 +79,7 @@ class PlanningCaptureAgent(CaptureAgent):
     values = [self.evaluate(gameState, a) for a in actions]
     # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
 
-    print "choice actions: " 
+    print "choice actions: "
     for i in range (0,len(actions)):
       print actions[i]
       print values[i]
@@ -127,9 +127,9 @@ class PlanningCaptureAgent(CaptureAgent):
       return successor
 
   def evaluate(self, gameState, action):
-    
+
     successor = self.getSuccessor(gameState, action)
-    
+
 
     """
     Computes a linear combination of features and feature weights
@@ -154,7 +154,7 @@ class PlanningCaptureAgent(CaptureAgent):
     """
     return {'successorScore': 1.0}
 
-  #TODO: 
+  #TODO:
   #first for the attacking agent
   def evaluateCurrentState(self, gameState):
     """
@@ -260,7 +260,7 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
     features = util.Counter()
 
     #sucessor is another gameState object of the next moment with a certain
-    #action 
+    #action
     successor = self.getSuccessor(gameState, action)
     foodList = self.getFood(successor).asList()
     features['successorScore'] = -len(foodList)#self.getScore(successor)
@@ -295,7 +295,7 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
       dangerGhostList.append(opponent)
     if len(dangerGhostList) > 0:
       features['isNearDangerGhost'] = 1
-    else: 
+    else:
       features['isNearDangerGhost'] = 0
     return features
     """
@@ -326,34 +326,9 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
 
     return features
 
-  def getFeatures(self, gameState, action):
-    features = util.Counter()
-    successor = self.getSuccessor(gameState, action)
-
-    myState = successor.getAgentState(self.index)
-    myPos = myState.getPosition()
-
-    # Computes whether we're on defense (1) or offense (0)
-    features['onDefense'] = 1
-    if myState.isPacman: features['onDefense'] = 0
-
-    # Computes distance to invaders we can see
-    enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-    invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
-    features['numInvaders'] = len(invaders)
-    if len(invaders) > 0:
-      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-      features['invaderDistance'] = min(dists)
-
-    if action == Directions.STOP: features['stop'] = 1
-    rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
-    if action == rev: features['reverse'] = 1
-
-    return features
-
   def getWeights(self, gameState, action):
     return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
- 
+
 
   #def getWeights(self, gameState, action):
    # return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
@@ -396,7 +371,7 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
     features = util.Counter()
 
     #sucessor is another gameState object of the next moment with a certain
-    #action 
+    #action
     foodList = self.getFood(gameState).asList()
     foodList1 = []
     foodList2 = []
@@ -415,12 +390,12 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
       if self.index < 2:
         if len(foodList1) > 0:
           minDistance = min([self.getMazeDistance(myPos, food) for food in foodList1])
-        else: 
+        else:
           minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
       else:
         if len(foodList2) > 0:
           minDistance = min([self.getMazeDistance(myPos, food) for food in foodList2])
-        else: 
+        else:
           minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
       features['distanceToFood'] = minDistance
 
@@ -448,9 +423,9 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
       dangerGhostList.append(opponent)
     if len(dangerGhostList) > 0:
       features['isNearDangerGhost'] = 1
-    else: 
+    else:
       features['isNearDangerGhost'] = 0
-      
+
     return features
 
 
@@ -465,7 +440,7 @@ class OffensivePlanningAgent(PlanningCaptureAgent):
 
 
 class DefensivePlanningAgent(PlanningCaptureAgent):
- 
+
 
   def getFeatures(self, gameState, action):
     features = util.Counter()
@@ -502,52 +477,3 @@ class DefensivePlanningAgent(PlanningCaptureAgent):
     features = self.getFeatures(gameState, action)
     weights = self.getWeights(gameState, action)
     return features * weights
-
-
-
-
-class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
-
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
-
-    '''
-    Your initialization code goes here, if you need any.
-    '''
-
-
-  def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
-    actions = gameState.getLegalActions(self.index)
-
-    '''
-    You should change this in your own agent.
-    '''
-
-    return random.choice(actions)
-
